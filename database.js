8f3b83f57eb1ceb870b73f835919b8e78857caa4
anmol0205 <anmol.gaur.ug22@nsut.ac.in>
2024-06-06 18:24:37 +0530

    second commit

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const uri = process.env.URI;
-console.log('MongoDB URI:', uri); // Add this line to debug
+
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(uri, {
+        await mongoose.connect("mongodb+srv://anmolgaur005:anmol2005@mycluster01.ebv16sh.mongodb.net/", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             serverSelectionTimeoutMS: 50000 // Increase timeout to 50 seconds
