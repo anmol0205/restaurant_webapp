9b47aab34587723c022f444331e2283bf0b4d750
anmol0205 <144688816+anmol0205@users.noreply.github.com>
2024-06-07 22:49:28 +0530

    Update database.js

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://anmolgaurug22:anmol2005@mycluster01.tsewde3.mongodb.net/", {
+        await mongoose.connect("mongodb+srv://anmolgaurug22:anmol2005@mycluster01.tsewde3.mongodb.net/Restaurant", {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             serverSelectionTimeoutMS: 50000 // Increase timeout to 50 seconds
